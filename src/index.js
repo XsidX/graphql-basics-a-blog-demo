0f488ba0284e14ee77901a78046dfa27dc887088
iamsid <developer.sid@outlook.com>
2021-05-26 15:20:33 +0300

    Graphql server setup( && my first graphql api)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log('Hello graphql')
+import { GraphQLServer } from 'graphql-yoga'
+
+//Type definitions [schema]--->operations ans datastructures
+
+const typeDefs = `
+    type Query{
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`
+    
+
+//Resolvers----->set of functions that run, query data
+
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query'
+        },
+        name() {
+            return 'Sidney Kaguli'
+        },
+        location() {
+            return 'Nairobi'
+        },
+        bio() {
+            return 'Startup founder'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() =>{
+    console.log('The server is up!')
+}) 
